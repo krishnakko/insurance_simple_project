55e567cc057bba2527cbc38fb8b43cb6bd509434
venkatakrishna.reddy@learnship.com <venkatakrishna.reddy@learnship.com>
2022-03-03 19:17:48 +0530

    small changes

--- insurance-client/src/components/Loader/loader.jsx
+++ insurance-client/src/components/Loader/loader.jsx
@@ -1,5 +1,5 @@
 import CircularProgress from '@material-ui/core/CircularProgress'
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
 import React from 'react';
 import "./loader.scss";
 
